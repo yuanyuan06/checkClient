208154041db69767ddddb78420de35ec5ac87378
yuanyuan06 <18801761215@163.com>
2016-01-19 17:13:49 +0800

    comment:none

--- src/main/java/com/jumbo/http/checkClient.java
+++ src/main/java/com/jumbo/http/checkClient.java
@@ -7,9 +7,5 @@ public class checkClient {
 
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
-
-
-//        Container registry = ExtensionLoader.getExtensionLoader(Container.class).getExtension("registry");
-//        System.out.println(registry);
     }
 }
