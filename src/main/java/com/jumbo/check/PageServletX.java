4294848dc5cfa813db0cd37d38059489e4d97c3a
yuanyuan06 <18801761215@163.com>
2016-01-19 17:08:22 +0800

    comment:none

---
+++ src/main/java/com/jumbo/check/PageServletX.java
@@ -0,0 +1,56 @@
+package com.jumbo.check;
+
+import com.alibaba.dubbo.common.URL;
+import org.mortbay.jetty.HttpException;
+import org.mortbay.jetty.HttpStatus;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Administrator on 2016/1/19.
+ */
+public class PageServletX extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    /**
+     * http://localhost:8080/host?host=2
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String query = req.getQueryString();
+        URL url = URL.valueOf(req.getRequestURL().toString() + (query == null || query.length() == 0 ? "" : "?" + query));
+        String host = url.getParameter("host");
+        ResourceBundle bundle = ResourceBundle.getBundle("hostConfig");
+        String host0 = bundle.getString("host" + host);
+
+        List<URL> providers = RegistryContainer.getInstance().getProvidersByHost(host0);
+        if(providers == null || providers.size() < 1){
+//            throw new HTTPException(HttpStatus.ORDINAL_404_Not_Found);
+            throw  new HttpException(HttpStatus.ORDINAL_404_Not_Found);
+        }
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("have services");
+        writer.flush();
+    }
+}
---
+++ src/main/java/com/jumbo/http/checkClient.java
@@ -0,0 +1,15 @@
+package com.jumbo.http;
+
+/**
+ * Created by Administrator on 2016/1/19.
+ */
+public class checkClient {
+
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+
+
+//        Container registry = ExtensionLoader.getExtensionLoader(Container.class).getExtension("registry");
+//        System.out.println(registry);
+    }
+}
