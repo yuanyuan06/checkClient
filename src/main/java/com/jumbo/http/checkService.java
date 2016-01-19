728ec4e1ed9ed327512572bec09f2f9921774cce
yuanyuan06 <18801761215@163.com>
2016-01-19 17:17:29 +0800

    comment:none

--- src/main/java/com/jumbo/http/checkService.java
+++ src/main/java/com/jumbo/http/checkService.java
@@ -3,7 +3,7 @@ package com.jumbo.http;
 /**
  * Created by Administrator on 2016/1/19.
  */
-public class checkClient {
+public class checkService {
 
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
