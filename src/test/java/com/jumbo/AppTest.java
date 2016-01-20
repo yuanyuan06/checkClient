cecd71d7aee451055720706ad86f6d719a845e69
yy <18801761215@163.com>
2016-01-20 21:56:36 +0800

    comment:none

--- src/test/java/com/jumbo/AppTest.java
+++ src/test/java/com/jumbo/AppTest.java
@@ -2,13 +2,23 @@ package com.jumbo;
 
 import org.junit.Test;
 
+import javax.swing.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
     @Test
-    public void testClient(){
+    public void testClient() throws IOException {
+        ServerSocket socket = new ServerSocket(12345);
+        Socket accept = socket.accept();
+
+        JOptionPane.showMessageDialog(null, "有客户端连接到本机12345");
+
 
     }
 }
