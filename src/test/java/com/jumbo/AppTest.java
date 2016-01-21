b10395f6cce3d1bd72b6efc81e7d9e4c87e9a099
yuanyuan06 <18801761215@163.com>
2016-01-21 12:43:45 +0800

    comment:none

--- src/test/java/com/jumbo/AppTest.java
+++ src/test/java/com/jumbo/AppTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 /**
  * Unit test for simple App.
@@ -14,12 +13,24 @@ public class AppTest
 {
     @Test
     public void testClient() throws IOException {
-        ServerSocket socket = new ServerSocket(12345);
-        Socket accept = socket.accept();
-
-        JOptionPane.showMessageDialog(null, "有客户端连接到本机12345");
-
-
+        Socket socket = new Socket();
+//        socket.setSoTimeout(1);
+        try{
+//            Socket socket = new Socket("www.baidu.com", 443);
+            SocketAddress addr = new InetSocketAddress("www.baidu.com", 4431);
+            socket.connect(addr, 3000);
+            boolean connected = socket.isConnected();
+            if(connected){
+                JOptionPane.showMessageDialog(null, "端口服务正常");
+            }else{
+                JOptionPane.showMessageDialog(null, "端口服务不存在");
+            }
+        }catch(ConnectException e){
+            JOptionPane.showMessageDialog(null, "端口服务不存在");
+        }catch (SocketTimeoutException e){
+            JOptionPane.showMessageDialog(null, "连接超时");
+        }finally{
+            socket.close();
+        }
     }
 }
-
